6521c2accfb36ceb39f5cb786a48e83c18d36b1c
William R. Ward <bill@komodo.(none)>
2011-02-01 13:10:15 -0800

    move js and css to static files

---
+++ static/js/new.js
@@ -0,0 +1,38 @@
+$(function() {
+  $("#datepicker").datepicker({minDate: 0});
+
+  try {
+    var formvalues = $.cookie('formvalues');
+    if (formvalues) {
+      formvalues = JSON.parse(formvalues);
+      $('#type-select').append('<option>'+formvalues['type']+'</option>');
+      for (var key in formvalues) {
+  if (key!="rooms") {
+    $('[name='+key+']').val(formvalues[key]);
+  }
+      }
+      $.each($('[name=rooms]'), function(key, value) {
+  if ($(value).val()==formvalues["rooms"]) {
+    $(value).attr("checked", "checked");
+  }
+      });
+    }
+    var formerror = $.cookie('formerror');
+    if (formerror) {
+      formerror = JSON.parse(formerror);
+      $('#warn').hide();
+      $('#error').show("slow").append("<b>Error:</b> "+ formerror);
+      $.cookie('formerror', null);
+    }
+  } catch (err) {
+    // noop
+  }
+
+});
+
+function showOther() {
+  $('#type-select').css('display','none');$('#type-text').css('display','inline');
+}
+function hideOther() {
+  $('#type-select').css('display','inline');$('#type-text').css('display','none');
+}
